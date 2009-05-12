a0cd1c013c7d146fd44f2bb725da7ad465269475
William Skellenger <wskellenger@gmail.com>
2009-05-12 10:14:04 -0400

    reorg comments in conf file

--- mpguino_conf.h
+++ mpguino_conf.h
@@ -1,13 +1,22 @@
 /* --- Compile-time options ---------------------------------- */
 
-#define BARGRAPH_DISPLAY_CFG                   0  // 0=Off 1=On
-#define CFG_BIGFONT_TYPE                       2  // 1=Default 2=Modified    (+24 bytes)
-#define CFG_FUELCUT_INDICATOR                  3  // 0=Off 1=simple star 2=spinner 3=bubble
-#define CFG_NICE_CURSOR                        0  // 0=Off 1=On              (+60 bytes)
-#define CFG_SERIAL_TX                          0  // 0=Off 1=On              (+232 bytes)
-#define SLEEP_CFG                              3  // 0=Off 1=bl 2=lcd 3=both (+64 bytes)
-#define TANK_IN_EEPROM_CFG                     1  // 0=Off 1=On
+#define BARGRAPH_DISPLAY_CFG                   0  
+// 0=Off 1=On
+#define CFG_BIGFONT_TYPE                       2  
+// 1=Default 2=Modified (+24 bytes)
+#define CFG_FUELCUT_INDICATOR                  3  
+// 0=Off 1=star (+54) 2=spinner 3=bubble (+74)
+#define CFG_NICE_CURSOR                        0  
+// 0=Off 1=On (+52)
+#define CFG_SERIAL_TX                          0  
+// 0=Off 1=On (+232)
+#define SLEEP_CFG                              3  
+// 0=Off 1=bl (+34) 2=lcd (+14) 3=both (+46)
+#define TANK_IN_EEPROM_CFG                     1  
+// 0=Off 1=On (+34)
 
 /* --- TODO --- */
-#define CFG_UNITS                              1  // 1=US  2=METRIC 
-#define MULTIPLE_VEHICLE_CFG                   0  // 0=Off 1=On
+#define CFG_UNITS                              1  
+// 1=US  2=METRIC 
+#define MULTIPLE_VEHICLE_CFG                   0  
+// 0=Off 1=On
